c1b96649f166f33b3120d07dedc384353122c3ce
Francisco Souza <francisco@franciscosouza.net>
2011-08-27 19:29:37 -0300

    Solution for exercise 1-12

--- 1-12.c
+++ 1-12.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 
+/*
+ * Write a program that print its input one word per line.
+ *
+ * How to test it (on Unix systems):
+ *
+ *      $ ./a.out < README
+ */
+
 int main (int argc, char const* argv[]) {
     char c;
     while ((c = getchar()) != EOF) {
