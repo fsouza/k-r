bfbfdce62fa8c2832db9b1a3cb1bda05db73a21e
Francisco Souza <francisco@franciscosouza.net>
2011-08-27 22:34:59 -0300

    Solution for exercise 1-18

---
+++ 1-18.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/*
+ * Write a program to remove trailing blanks and tabs
+ * from each line of input, and to delete entirely
+ * blank lines.
+ *
+ * How to test it (on Unix systems):
+ *
+ *      $ ./a.out < 1-18.in
+ *
+ */
+
+#define MAXLINE 1000
+
+int main (int argc, char const* argv[]) {
+    char c, line[MAXLINE];
+    int i, last_nonblank;
+
+    while ((c = getchar()) != EOF) {
+        last_nonblank = 0;
+        for (i = 0; (c = getchar()) != EOF && c != '\n'; i++) {
+            line[i] = c;
+            if (c != ' ' && c != '\t') {
+                last_nonblank = i;
+            }
+        }
+
+        if (last_nonblank > 0) {
+            line[last_nonblank + 1] = '\0';
+            printf("%s\n", line);
+        }
+    }
+
+    return 0;
+}
